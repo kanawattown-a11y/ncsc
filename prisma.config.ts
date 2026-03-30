57ddcb524361cc28d1bfa382cfe0d5766f1c233c
kanawattown-a11y <kanawattown@gmail.com>
2026-03-30 13:33:29 +0400

    ncsc 4

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,17 +1,12 @@
 try {
   require("dotenv").config();
-} catch (e) {
-  // dotenv might be missing in some environments, Prisma usually loads .env anyway
-}
+} catch (e) {}
 
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
   datasource: {
-    url: process.env["DATABASE_URL"],
+    url: process.env.DATABASE_URL,
   },
 });
